5d251fe54c73bc47310e419ed95bef8f3b609645
Son Lam <son.lamvan@nashtechglobal.com>
2019-12-25 18:08:55 +0700

    37: 2'50

--- src/App.vue
+++ src/App.vue
@@ -1,56 +1,60 @@
 <template>
-	<div id="app">
-		<!-- <CompHeader></CompHeader> -->
-		<!-- <CompHeader /> -->
-		<comp-header />
-		<img src="./assets/logo.png">
-		<h1>{{ msg }}</h1>
-		<comp-footer />
-	</div>
+  <div id="app">
+    <div class="wrapper clearfix">
+      <players v-bind:scorePlayer="scorePlayer" />
+
+      <controls />
+
+      <dices />
+
+      <input type="number" placeholder="Final score" class="final-score" />
+    </div>
+  </div>
 </template>
 
 <script>
-import CompHeader from './components/CompHeader.vue';
-import CompFooter from './components/CompFooter.vue';
+import Players from "./components/Players";
+import Controls from "./components/Controls";
+import Dices from "./components/Dices";
 export default {
-	name: 'app',
-	data () {
-		return {
-			msg: 'Hello VueJs'
-		}
-	},
-	components: {
-		CompHeader,
-		CompFooter
-	}
-}
+  name: "app",
+  data() {
+    return {};
+  },
+  components: {
+    Players,
+    Controls,
+    Dices
+  }
+};
 </script>
 
 <style>
-	#app {
-		font-family: 'Avenir', Helvetica, Arial, sans-serif;
-		-webkit-font-smoothing: antialiased;
-		-moz-osx-font-smoothing: grayscale;
-		text-align: center;
-		color: #2c3e50;
-		margin-top: 60px;
-	}
+#app {
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
 
-	h1, h2 {
-		font-weight: normal;
-	}
+h1,
+h2 {
+  font-weight: normal;
+}
 
-	ul {
-		list-style-type: none;
-		padding: 0;
-	}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
 
-	li {
-		display: inline-block;
-		margin: 0 10px;
-	}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
 
-	a {
-		color: #42b983;
-	}
+a {
+  color: #42b983;
+}
 </style>
---
+++ src/components/Dices.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="wrapper-dices">
+    <div id="dice-1" class="dice">
+      <div class="spinner dice-3">
+        <div class="face1">1</div>
+        <div class="face2">2</div>
+        <div class="face3">3</div>
+        <div class="face4">4</div>
+        <div class="face5">5</div>
+        <div class="face6">6</div>
+      </div>
+    </div>
+    <div id="dice-2" class="dice">
+      <div class="spinner dice-6">
+        <div class="face1">1</div>
+        <div class="face2">2</div>
+        <div class="face3">3</div>
+        <div class="face4">4</div>
+        <div class="face5">5</div>
+        <div class="face6">6</div>
+      </div>
+    </div>
+  </div></template
+>
+
+<script>
+export default {
+  name: "dices",
+  data() {
+    return {};
+  }
+};
+</script>
+
+<style></style>
